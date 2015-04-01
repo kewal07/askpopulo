9409154e50f6528321961894a2a59bdb56f6c873
kewal07 <kewal07@gmail.com>
2015-04-01 20:16:18 +0530

    Added login and basic ui

--- askpopulo/urls.py
+++ askpopulo/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+	url(r'^accounts/', include('allauth.urls')),
+	url(r'^', include('polls.urls',namespace="polls")),
 )
