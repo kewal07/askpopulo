78f3a9386c8cd2a8ae9ec76782016ac4042ef929
kewal07 <kewal07@gmail.com>
2015-04-08 02:06:03 +0530

    profile integration

--- login/models.py
+++ login/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.conf import settings
+#from django.contrib.auth.models import User
 # Create your models here.
+
+class ExtendedUser(models.Model):
+	user = models.OneToOneField(settings.AUTH_USER_MODEL)
+	imageUrl = models.CharField(max_length=512,blank=True,null=True)
+	birthDay = models.DateField(default="2002-01-01")
+	gender = models.CharField(max_length=1,blank=True,null=True)
+	city = models.CharField(max_length=512,blank=True,null=True)
+	state = models.CharField(max_length=512,blank=True,null=True)
+	country = models.CharField(max_length=512,blank=True,null=True)
+	bio = models.CharField(max_length=1024,blank=True,null=True)
+	profession = models.CharField(max_length=512,blank=True,null=True)
--- login/views.py
+++ login/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.views import generic
+from django.conf import settings
+from login.models import ExtendedUser
 
-# Create your views here.
+class LoggedInView(generic.ListView):
+	template_name = 'login/profile.html'
+	model = settings.AUTH_USER_MODEL
+	# context_object_name = 'data'
+	# print(request.path)
+	# template_name=request.path
+	def get_queryset(self):
+		user = self.request.user
+		print(user)
+		print(dir(user))
+		social_set = user.socialaccount_set.all()[0]
+		print((social_set.extra_data))
+		if not (ExtendedUser.objects.filter(user_id = user.id)):
+			if social_set.provider == 'facebook':
+				facebook_data = social_set.extra_data
+				img_url =  "http://graph.facebook.com/{}/picture?width=140&&height=140".format(facebook_data.get('id',''))
+				gender_data = facebook_data.get('gender','')[0].upper()
+				birth_day = facebook_data.get('birthday','2002-01-01')
+				extendedUser = ExtendedUser(user=user, imageUrl = img_url, birthDay = birth_day,gender=gender_data)
+				extendedUser.save()
+		
+		
+# class DetailView(generic.DetailView):
+	# template_name = 'polls/index.html'
