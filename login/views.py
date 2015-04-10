2ae048a192099fe8e6ac80a80c98c8e8fa6f9980
shradha <shradhagautam15@gmail.com>
2015-04-10 22:02:43 +0530

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	askpopulo/__pycache__/settings.cpython-34.pyc
    	login/__pycache__/models.cpython-34.pyc

--- login/views.py
+++ login/views.py
@@ -11,8 +11,8 @@ class LoggedInView(generic.ListView):
 	# template_name=request.path
 	def get_queryset(self):
 		user = self.request.user
-		print(user)
-		print(dir(user))
+		#print(user)
+		#print(dir(user))
 		social_set = user.socialaccount_set.all()[0]
 		print((social_set.extra_data))
 		if not (ExtendedUser.objects.filter(user_id = user.id)):
@@ -23,7 +23,18 @@ class LoggedInView(generic.ListView):
 				birth_day = facebook_data.get('birthday','2002-01-01')
 				extendedUser = ExtendedUser(user=user, imageUrl = img_url, birthDay = birth_day,gender=gender_data)
 				extendedUser.save()
-		
+			if social_set.provider == 'google':
+				google_data = social_set.extra_data
+				img_url = google_data.get('picture')
+				gender_data = google_data.get('gender','')[0].upper()
+				extendedUser = ExtendedUser(user=user, imageUrl = img_url, gender=gender_data)
+				extendedUser.save()
+			if social_set.provider == 'twitter':
+				twitter_data = social_set.extra_data
+				img_url = twitter_data.get('profile_image_url')
+				city_data = twitter_data.get('location','')
+				extendedUser = ExtendedUser(user=user, imageUrl = img_url, city=city_data)
+				extendedUser.save()
 		
 # class DetailView(generic.DetailView):
 	# template_name = 'polls/index.html'
