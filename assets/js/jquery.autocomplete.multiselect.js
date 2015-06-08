3d304ff52eeca4b683c1a54b2e3aa7de567cbb35
Ubuntu <ubuntu@ip-172-31-34-93.us-west-2.compute.internal>
2015-06-08 21:34:34 +0000

    finally the rock is home

---
+++ assets/js/jquery.autocomplete.multiselect.js
@@ -0,0 +1,116 @@
+// http://jsfiddle.net/mekwall/sgxKJ/
+
+$.widget("ui.autocomplete", $.ui.autocomplete, {
+    options : $.extend({}, this.options, {
+        multiselect: false,
+        maxselection: 5
+    }),
+    _create: function(){
+        this._super();
+        var itemsAdded = $('#selectedCategories').val();
+        var self = this,
+            o = self.options;
+
+        if (o.multiselect) {
+            console.log('multiselect true');
+            self.selectedItems = {};           
+            self.multiselect = $("<div></div>")
+                .addClass("ui-autocomplete-multiselect ui-state-default ui-widget")
+                .css("width", self.element.width())
+                .insertBefore(self.element)
+                .append(self.element)
+                .bind("click.autocomplete", function(){
+                    self.element.focus();
+                });
+				
+			if(!(itemsAdded === "")){
+				console.log(itemsAdded.split(","));
+				$.each(itemsAdded.split(","),function(i,items){
+					console.log(items);
+					if(!(items === "")){
+						o.maxselection--;
+						$("<div></div>")
+							.addClass("ui-autocomplete-multiselect-item")
+							.text(items)
+							.append(
+								$("<span></span>")
+									.addClass("ui-icon ui-icon-close")
+									.click(function(){
+										o.maxselection++;
+										var item = $(this).parent();
+										delete self.selectedItems[item.text()];
+										itemsAdded=itemsAdded.replace(item.text()+",","");
+										$('#selectedCategories').val(itemsAdded);
+										item.remove();
+									})
+							)
+							.insertBefore(self.element);
+					}
+				});
+			}
+            
+            var fontSize = parseInt(self.element.css("fontSize"), 10);
+            function autoSize(e){
+                // Hackish autosizing
+                var $this = $(this);
+                $this.width(1).width(this.scrollWidth+fontSize-1);
+            };
+
+            var kc = $.ui.keyCode;
+            self.element.bind({
+                "keydown.autocomplete": function(e){
+                    if ((this.value === "") && (e.keyCode == kc.BACKSPACE)) {
+                        var prev = self.element.prev();
+                        delete self.selectedItems[prev.text()];
+                        itemsAdded=itemsAdded.replace(prev.text()+",","");
+                        $('#selectedCategories').val(itemsAdded);
+                        o.maxselection++;
+                        prev.remove();
+                    }
+                },
+                // TODO: Implement outline of container
+                "focus.autocomplete blur.autocomplete": function(){
+                    self.multiselect.toggleClass("ui-state-active");
+                },
+                "keypress.autocomplete change.autocomplete focus.autocomplete blur.autocomplete": autoSize
+            }).trigger("change");
+            // TODO: There's a better way?
+            o.select = o.select || function(e, ui) {
+                if(!(ui.item.label in self.selectedItems) && o.maxselection > 0){
+                    itemsAdded = itemsAdded+(ui.item.label+",");//append(ui.item.label+",");
+                    $('#selectedCategories').val(itemsAdded);
+                    o.maxselection--;
+                    $("<div></div>")
+                        .addClass("ui-autocomplete-multiselect-item")
+                        .text(ui.item.label)
+                        .append(
+                            $("<span></span>")
+                                .addClass("ui-icon ui-icon-close")
+                                .click(function(){
+                                    o.maxselection++;
+                                    var item = $(this).parent();
+                                    delete self.selectedItems[item.text()];
+                                    itemsAdded=itemsAdded.replace(item.text()+",","");
+                                    $('#selectedCategories').val(itemsAdded);
+                                    item.remove();
+                                })
+                        )
+                        .insertBefore(self.element);
+                }
+                else if(o.maxselection <= 0)
+                {
+                    alert("Maximum number of categories selected");
+                }
+                self.selectedItems[ui.item.label] = ui.item;
+                self._value("");
+                 return false;
+            }
+            /*self.options.open = function(e, ui) {
+                var pos = self.multiselect.position();
+                pos.top += self.multiselect.height();
+                self.menu.element.position(pos);
+            }*/
+        }
+        return this;
+    }
+});
---
+++ assets/js/script.js
@@ -0,0 +1,170 @@
+$(document).ready(function(){
+	$("#id_categories").addClass("clearfix");
+	
+	if($(window).width() <= 960){
+		$("#verticalTab").addClass("responsiveStats");
+		$("#tabContentResponsive").show();
+	}
+
+	/*gender css for responsive view*/
+	// console.log($("label[for='id_gender_0']").parent().children().first().next());
+	// $("label[for='id_gender_0']").parent().children().first().next().css({"display":"inline-block !important","width":"33% !important"});
+	$("label[for='id_gender_0']").parent().children().first().next().addClass("first_Gender");
+	/*end*/
+	
+	/*For file browse */
+	var invisible = $('<div/>').css({height:0,width:0,'overflow':'hidden','display':'inline-block'});
+	var label = $('<div class="fileLabel"><img class="upImg" ><span id="upImgText">Upload Image</span></div>');
+	// var upImg = $('<img id="upImg" width=>')
+	var fileInput = $(":file").after(label).wrap(invisible);
+	$(".upImg").hide();
+
+	fileInput.change(function(){
+		// $('#upImgText').hide();
+	    $this = $(this);
+	    var fileVal = $this.val();
+    	var fileNameIndex = fileVal.lastIndexOf("\\") + 1;
+    	var fileName = fileVal.substr(fileNameIndex);
+    	if (this.files && this.files[0]) {
+            var reader = new FileReader();
+            
+            reader.onload = function (e) {
+                imgSrc = e.target.result;
+				console.log($this.parent().children().first());
+				// $(".upImg").attr('src', e.target.result);
+				$this.parent().next().children().first().attr('src', e.target.result);
+				// $(".upImg").show();
+				$this.parent().next().children().first().show();
+            }
+            
+            reader.readAsDataURL(this.files[0]);
+        }
+		// $("#upImgText").text(fileName);
+		$this.parent().next().children().first().next().hide();
+    	$this.parent().parent().children().first().next().attr("placeholder", "Describe your image");
+	})
+
+	$('.fileLabel').click(function(){
+	    $(this).prev().children().first().click();
+	});
+	/* End of file browse*/
+
+	/* For changing border color of the selected radio image */
+	$(".choices").click(function(){
+		// console.log($(".choices.choice_image#"+$(this)[0].id)[0]);
+		$(".choice_image").css({"border-color":"#666"});
+		($(".choice_image#"+$(this)[0].id)[0]).style.borderColor="#00FF00";
+	});
+	/* End of changing image border color */
+
+	/* Dropdownbox on click of user image in nav */
+	$('.userInNav').click(function(){
+		$('.dropDownBox').slideToggle("slow");
+	});
+	$('.menuResponsive').click(function(){
+		$('.dropDownBox').slideToggle("slow");
+	});
+	/* end of Dropdownbox on click of user image in nav */
+	
+	/* Overlay */
+	/* close overlay call */
+	$("#okay").click(function(){
+		closeOverlay();
+	});
+	/* close overlay call end */
+
+	$('#id_categories').hide();
+	$('#id_categories').prev().append("<span id='cats'>Please Click here to expand and select your categories</span>");
+	$('#id_categories').prev().on('click', '#cats', function() {
+    	$('#id_categories').toggle();
+  	});
+
+	/*help*/
+	$(".helpChardin").click(function(e){
+		e.preventDefault();
+		$('html, body').animate({ scrollTop: 0 }, 'slow');
+		$('body').chardinJs('start');
+	});
+	/*end*/
+	
+	/* agreement in sign up page */
+	var agreement_label = '<label class="agreement_label" for="id_agreement">I have read and agree with the <a class="agreement_anchor" href="http://127.0.0.1:8000/static/AskByPollTermsofUse.docx" target="_blank">Terms of Use</a> and <a class="agreement_anchor" href="http://127.0.0.1:8000/static/ASKBYPOLLPrivacyPolicies.docx" target="_blank">Privacy</a></label>'
+	$("#id_agreement").parent().append(agreement_label);
+	/* end */
+
+	/* activate menu link */
+	activateMenuLink();
+	/* end */
+
+	/* description on hover */
+	if($(window).width() > 960){
+		$(".qTextDesc").hover(function(){
+			var elem=$(this)[0];
+			if($("#"+elem.id).next().text().length > 0)
+				$("#"+elem.id).next().toggle();
+		});
+	}
+	/* end */
+});
+
+function activateMenuLink () {
+	$( ".menuanchor" ).each(function( ) {
+		if($(this)[0].href == window.location.href)
+			$(this).addClass('anchoractive');
+	});
+}
+function openOverlay(olEl) {
+	console.log("In overlay");
+	$oLay = $(olEl);
+	if ($('#overlay-shade').length == 0)
+		$('body').prepend('<div id="overlay-shade"></div>');
+	$('#overlay-shade').fadeTo(300, 0.6, function() {
+		// var props = {
+			// //oLayWidth       : $oLay.width(),
+			// scrTop          : $(window).scrollTop(),
+			// viewPortWidth   : $(window).width()
+		// };
+	$oLay.css({
+			display : 'block',
+			opacity : 0,
+			});
+	$oLay.animate({
+			top : 130,
+			opacity : 1
+			}, 600);
+	});
+}
+
+function closeOverlay() {
+	$('.overlay').animate({
+		top : '-=300',
+		opacity : 0
+	}, 400, function() {
+		$('#overlay-shade').fadeOut(300);
+		$(this).css('display','none');
+	});
+}
+/* End Overlay */
+
+function yesnoconfirm(url){
+	$("#yes").click(function(){
+		closeOverlay();
+		window.location.href = url;
+	});
+	$("#no").click(function(){
+		closeOverlay();
+		return false;
+	});
+}
+
+function confirm_redirect(olEl,val,url){
+	$oLay = $(olEl);
+	console.log(url);
+	// console.log(val);
+	if (val === "delete_question"){
+		console.log($oLay.children().children().first()[0])
+		$oLay.children().children().first()[0].innerHTML = "You Sure Want to Delete the Poll??";
+	}
+	openOverlay(olEl);
+	return yesnoconfirm(url);
+}
