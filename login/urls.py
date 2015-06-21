b1c9b69feeab301f68d1b39540ec3d1c298c6206
Ubuntu <ubuntu@ip-172-31-34-93.us-west-2.compute.internal>
2015-06-21 07:26:30 +0000

    server changes

--- login/urls.py
+++ login/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from login import views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,10 +8,10 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^', views.index),
-    url(r'^(?P<pk>\d+)/(?P<user_slug>[\w\-]+)/$',views.LoggedInView.as_view(),name='loggedIn'),
-	url(r'^$',views.RedirectLoginView.as_view(),name='loginRedirect'),
-	url(r'^editprofile$',views.EditProfileView.as_view(),name='edit_profile'),
+    url(r'^(?P<pk>\d+)/(?P<user_slug>[\w\-]+)/$',login_required(views.LoggedInView.as_view()),name='loggedIn'),
+	url(r'^$',login_required(views.RedirectLoginView.as_view()),name='loginRedirect'),
+	url(r'^editprofile$',login_required(views.EditProfileView.as_view()),name='edit_profile'),
 	url(r'^logout$',views.logout_view,name="logout"),
-	url(r'^changepassword$',views.MyChangePasswordView.as_view(),name="change_password"),
+	url(r'^changepassword$',login_required(views.MyChangePasswordView.as_view()),name="change_password"),
 	# url(r'^/detail$',views.DetailView.as_view(),name='detail'),
 )
