5ed5f9e34ac11d8f8dc7d2085838855c4fcd6241
shradhagautam15 <shradhagautam15@gmail.com>
2015-07-04 01:27:08 +0530

    change password ajax redirect https error solved

--- login/views.py
+++ login/views.py
@@ -154,14 +154,17 @@ class MyChangePasswordView(PasswordChangeView):
 	def post(self,request,*args,**kwargs):
 		form_class = super(MyChangePasswordView, self).get_form_class()
 		form = self.get_form(form_class)
+		data={}
 		# print(form.save())
 		if form.is_valid():
 			super(MyChangePasswordView, self).form_valid(form)
 		else:
 			# response = self.form_invalid(form)
 			# response.template_name = "login/profile.html"
-			data={}
 			data['form_errors'] = form._errors
+			return HttpResponse(json.dumps(data),
+                            content_type='application/json')
+		if request.is_ajax():
 			return HttpResponse(json.dumps(data),
                             content_type='application/json')
 		url = reverse('account_login')
