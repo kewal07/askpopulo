45055ae85e316ab29fd074f165da1edf50b0c537
kewal07 <kewal07@gmail.com>
2016-03-17 15:52:49 +0530

    font decreased

--- polls/abp_business_constants.py
+++ polls/abp_business_constants.py
@@ -1,16 +1,13 @@
 solution_caption = [
 	{'enterprise' : {
-		"caption": "Meaningful Insights<br>Advanced Analysis<br>Meaningful Insights",
-		"brief":"With the Hyper Connected World, it is becoming essential for enterprises to constantly be in touch with their customers as well as employees. Gathering feedback from customers and employees is of critical business value. Use the AskByPoll Products and Services to gather opinion from Customers and Employees at the click of a few buttons.This is how we can help - ",
+		"caption": "Customer Satisfaction<br>Employee Satisfaction<br>Employee Engagement",
+		"brief":"Many companies follow a Customer first Approach whereas there a lot many which follow an Employee first approach as well. With the Hyper Connected World, it is becoming essential for Enterprises to constantly be in touch with their Customers as well as Employees. Use the AskByPoll Products and Services to measure Employee & Customer Satisfaction at the click of a few buttons. This is how we can help –",
 		"details": [
-					"Gather valuable data from customer feedback to product research",
-					"E-Mails and Website Widgets.Product Specific Feedback from within your Website" 
-					"Use advanced analytics to turn your data into smarter business decisions",
-					"Customised NPS Study, In-person Interviews and Analysis",
-					"Employee Satisfaction Surveys to gauge the Employee Engagement",
-					"Fun Filled Polls to Connect with Employees on Various Matters",
-					"Innovative Employee Interaction and Engagement Systems",
-					"Support for Multiple Admin User Accounts ",
+					"Customized NPS Survey with In-Person Interviews and Analysis",
+					"Gather Business Critical Data from Customers Through Product Feedback Widgets" 
+					"Offload Your End to End Employee Satisfaction Measurement and Analysis Process",
+					"Increase Employee Engagement by Involving them in Decision Making & Activity Planning",
+					"Use our Technology for Transport Service, Caterer, Work Life Balance, Cultural Feedback",
 					"Gain access to AskByPoll Respondent Community ",
 					"Integrate with your existing systems",
 					"Secure Data, Always Yours",
@@ -18,16 +15,14 @@ solution_caption = [
 					]
 	}},
 	{'government' : {
-		"caption": "Meaningful Insights<br>Advanced Analysis<br>Meaningful Insights",
+		"caption": "Public Opinion<br>Social Interactions<br>Planning Data",
 		"brief":"With Citizen Involvement becoming more and more important in Public Policy Matters, it is becoming essential for the Governments to device ways to easily get in touch with the Citizens to gather their opinion. Use the AskByPoll Products and Services to gather opinion from Citizens at the click of a few buttons. This is how we can help –",
 		"details": [
-					"Gather Citizen Feedback Using Our Social Campaign Tool & Email Campaigns",
-					"Use the Power of Social Polls to Talk to Citizens to Seek Their Opinion",
-					"Employee Satisfaction Surveys to gauge the Employee Engagement",
-					"Fun Filled Polls to Connect with Employees on Various Matters",
-					"Innovative Employee Interaction and Engagement Systems",
-					"End-to-End Research Studies"
-					"Support for Multiple Admin User Accounts ",
+					"Customized NPS Survey with In-Person Interviews and Analysis",
+					"Gather Business Critical Data from Customers Through Product Feedback Widgets" 
+					"Offload Your End to End Employee Satisfaction Measurement and Analysis Process",
+					"Increase Employee Engagement by Involving them in Decision Making & Activity Planning",
+					"Use our Technology for Transport Service, Caterer, Work Life Balance, Cultural Feedback",
 					"Gain access to AskByPoll Respondent Community ",
 					"Integrate with your existing systems",
 					"Secure Data, Always Yours",
@@ -35,15 +30,14 @@ solution_caption = [
 					]
 	}},
 	{'startups' : {
-		"caption": "Meaningful Insights<br>Advanced Analysis<br>Meaningful Insights",
-		"brief":" Starting something of your own is always thrilling. As and when you start planning or even after you have started finding your foot in the vast ocean of business it is extremely critical to say in touch with your customers and take their feedback as well as validate and cross check the business model to gradually move towards success.   Use the AskByPoll Products and Services to gather opinion from customers and validate your business at the click of a few buttons. This is how we can help –",
+		"caption": "Market Validation<br>App / Website User Testing<br>Market Attractiveness",
+		"brief":" Starting something of your own is always thrilling. As and when you start planning or even after you have started finding your foot in the vast ocean of business it is extremely critical to say in touch with your customers and take their feedback as well as validate and cross check the business model to gradually move towards success. Use the AskByPoll Products and Services to gather opinion from customers and validate your business at the click of a few buttons. This is how we can help –",
 		"details": [
-					"Market Validation Service for Startups",
-					"Use advanced analytics to turn your data into smarter business decisions",
-					"Customer Feedback System",
-					"Employee Satisfaction Surveys",
-					"Social Polls to Improve Customer Engagement",
-					"Support for Multiple Admin User Accounts ",
+					"Get your Business Model Validated With Actual Customers",
+					"Get help with Pre-Launch App and Website Design & Functionality User Testing" 
+					"Gauge the Attractiveness of Your Product or Service with Your Target Market Group",
+					"Gather Customer Feedback After Launching",
+					"Improve Your Customer Engagement using Social Polls & Campaigns",
 					"Gain access to AskByPoll Respondent Community ",
 					"Integrate with your existing systems",
 					"Secure Data, Always Yours",
@@ -51,7 +45,7 @@ solution_caption = [
 					]
 	}},
 	{'NGOs' : {
-		"caption": "Meaningful Insights<br>Advanced Analysis<br>Meaningful Insights",
+		"caption": "Impact Assessment<br>Social Campaigns<br>Issue Sensitivity",
 		"brief":" Starting something of your own is always thrilling. As and when you start planning or even after you have started finding your foot in the vast ocean of business it is extremely critical to say in touch with your customers and take their feedback as well as validate and cross check the business model to gradually move towards success.   Use the AskByPoll Products and Services to gather opinion from customers and validate your business at the click of a few buttons. This is how we can help –",
 		"details": [
 					"Impact Assessment Services",
@@ -91,7 +85,7 @@ solution_caption = [
 					]
 	}},
 	{'academics' : {
-		"caption": "Meaningful Insights<br>Advanced Analysis<br>Meaningful Insights",
+		"caption": "Student Faculty Interactions<br>Alumni Feedback<br>Academic Research",
 		"brief":" Starting something of your own is always thrilling. As and when you start planning or even after you have started finding your foot in the vast ocean of business it is extremely critical to say in touch with your customers and take their feedback as well as validate and cross check the business model to gradually move towards success.   Use the AskByPoll Products and Services to gather opinion from customers and validate your business at the click of a few buttons. This is how we can help –",
 		"details": [
 					"Gather valuable data from customer feedback to product research",
@@ -113,7 +107,7 @@ solution_caption = [
 					]
 	}},
 	{'healthcare' : {
-		"caption": "Meaningful Insights<br>Advanced Analysis<br>Meaningful Insights",
+		"caption": "Quality Control<br>Patient Feedback<br>Vendor Feedback",
 		"brief":" Starting something of your own is always thrilling. As and when you start planning or even after you have started finding your foot in the vast ocean of business it is extremely critical to say in touch with your customers and take their feedback as well as validate and cross check the business model to gradually move towards success.   Use the AskByPoll Products and Services to gather opinion from customers and validate your business at the click of a few buttons. This is how we can help –",
 		"details": [
 					"Gather valuable data from customer feedback to product research",
@@ -135,29 +129,22 @@ solution_caption = [
 					]
 	}},
 	{'e-commerce' : {
-		"caption": "Meaningful Insights<br>Advanced Analysis<br>Meaningful Insights",
-		"brief":" Starting something of your own is always thrilling. As and when you start planning or even after you have started finding your foot in the vast ocean of business it is extremely critical to say in touch with your customers and take their feedback as well as validate and cross check the business model to gradually move towards success.   Use the AskByPoll Products and Services to gather opinion from customers and validate your business at the click of a few buttons. This is how we can help –",
+		"caption": "Consumer Insights<br>Product Reviews<br>Inventory Tagging",
+		"brief":" E-Commerce is one of the fastest Growing Sectors Today. Not only does it bring in convenience for customers, but also ensures easier availability, choice in options, flexible purchase & return and most importantly ability to choose the right Product based on reviews and ratings. Hence, it becomes critical for E-Commerce players to have robust systems to gather feedback on their Products from their customers as well as develop capabilities to gather opinion from a larger open Audience regarding their preferences.  Use the AskByPoll Products and Services to gather opinion from customers at the click of a few buttons. This is how we can help –",
 		"details": [
-					"Gather valuable data from customer feedback to product research",
-					"Use advanced analytics to turn your data into smarter business decisions",
-					"Create sophisticated surveys in minutes",
-					"Customised NPS Study, In-person Interviews and Analysis",
-					"Own your organization’s data regardless of personnel changes",
-					"Easily manage multiple users—allow single sign on through your network",
-					"Protect data with advanced SSL security and HIPAA-compliant features",
-					"Gain access to millions of respondents with AskByPoll Audience",
-					"Innovative Employee Interaction and Engagement Mechanisms",
-					"Customised Website and Email Widgets",
-					"Easily manage multiple users—allow single sign on through your network",
-					"Protect data with advanced SSL security and HIPAA-compliant features",
-					"Gain access to millions of respondents with AskByPoll Audience",
-					"Innovative Employee Interaction and Engagement Mechanisms",
-					"Customised Website and Email Widgets",
+					"Gather Preference Insights From Consumers Outside Your Own Customer Base",
+					"Perform Product Reviews to Understand Category and SKU Specific Preferences" 
+					"Offload Your Category Tagging Activities to our Online Community",
+					"Plan your Inventory Better By Understanding demand Preferences of People",
+					"New Product or Feature Launch Validation & User Testing before Making it Public",
+					"Gain access to AskByPoll Respondent Community ",
+					"Integrate with your existing systems",
+					"Secure Data, Always Yours",
 					"Get expert advice and 24/7 support any time you need"
 					]
 	}},
 	{'hospitality' : {
-		"caption": "Meaningful Insights<br>Advanced Analysis<br>Meaningful Insights",
+		"caption": "Customer Feedback<br>Public Opinion<br>Vendor Feedback",
 		"brief":" Starting something of your own is always thrilling. As and when you start planning or even after you have started finding your foot in the vast ocean of business it is extremely critical to say in touch with your customers and take their feedback as well as validate and cross check the business model to gradually move towards success.   Use the AskByPoll Products and Services to gather opinion from customers and validate your business at the click of a few buttons. This is how we can help –",
 		"details": [
 					"Gather valuable data from customer feedback to product research",
@@ -179,29 +166,21 @@ solution_caption = [
 					]
 	}},
 	{'publishers' : {
-		"caption": "Meaningful Insights<br>Advanced Analysis<br>Meaningful Insights",
-		"brief":" Starting something of your own is always thrilling. As and when you start planning or even after you have started finding your foot in the vast ocean of business it is extremely critical to say in touch with your customers and take their feedback as well as validate and cross check the business model to gradually move towards success.   Use the AskByPoll Products and Services to gather opinion from customers and validate your business at the click of a few buttons. This is how we can help –",
+		"caption": "Increased User Engagement<br>Public Opinion<br>Sensitivity Campaigns ",
+		"brief":" One of the most Competitive Landscapes existing today, Publishing thrives on User Traction and Engagement.  More engaging the content and more interactive the interfaces between the content and consumers better the chances of success for the Publisher.  Use the AskByPoll Products and Services to gather Public Opinion at the click of a few buttons. This is how we can help –",
 		"details": [
-					"Gather valuable data from customer feedback to product research",
-					"Use advanced analytics to turn your data into smarter business decisions",
-					"Create sophisticated surveys in minutes",
-					"Customised NPS Study, In-person Interviews and Analysis",
-					"Own your organization’s data regardless of personnel changes",
-					"Easily manage multiple users—allow single sign on through your network",
-					"Protect data with advanced SSL security and HIPAA-compliant features",
-					"Gain access to millions of respondents with AskByPoll Audience",
-					"Innovative Employee Interaction and Engagement Mechanisms",
-					"Customised Website and Email Widgets",
-					"Easily manage multiple users—allow single sign on through your network",
-					"Protect data with advanced SSL security and HIPAA-compliant features",
-					"Gain access to millions of respondents with AskByPoll Audience",
-					"Innovative Employee Interaction and Engagement Mechanisms",
-					"Customised Website and Email Widgets",
+					"Improving User Engagement by letting Consumers interact with Content",
+					"Gather Public Opinion Easily and Use the Information for Content Creation",
+					"Create Engaging Campaigns to Measure Public Sentiments on Interest Issues",
+					"Improve Your Data Validation by Gathering Primary Consumer Data Easily",
+					"Gain access to AskByPoll Respondent Community ",
+					"Integrate with your existing systems",
+					"Secure Data, Always Yours",
 					"Get expert advice and 24/7 support any time you need"
 					]
 	}},
-	{'research' : {
-		"caption": "Meaningful Insights<br>Advanced Analysis<br>Meaningful Insights",
+	{'fmcg' : {
+		"caption": "Consumer Research<br>Customer Engagement<br>Planning Data",
 		"brief":" Starting something of your own is always thrilling. As and when you start planning or even after you have started finding your foot in the vast ocean of business it is extremely critical to say in touch with your customers and take their feedback as well as validate and cross check the business model to gradually move towards success.   Use the AskByPoll Products and Services to gather opinion from customers and validate your business at the click of a few buttons. This is how we can help –",
 		"details": [
 					"Gather valuable data from customer feedback to product research",
