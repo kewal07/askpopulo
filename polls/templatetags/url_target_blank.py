99453053395b5a9eb4cbe0a3592b29ccce363753
Ankit Goyal <goyal.ankit.049@gmail.com>
2016-05-06 15:21:54 +0530

    templatetag for urlize

---
+++ polls/templatetags/url_target_blank.py
@@ -0,0 +1,9 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+
+register = template.Library()
+
+@register.filter(name='url_target_blank', is_safe=True)
+def url_target_blank(text):
+	return text.replace('<a ', '<a target="_blank" ')
+url_target_blank = register.filter(url_target_blank)
