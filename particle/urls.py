fcb6890022f7173e4e9bccab4c5cf2adee08de78
Ankit Goyal <goyal.ankit.049@gmail.com>
2016-05-21 11:09:22 +0530

    survey invitation email change to aws

--- particle/urls.py
+++ particle/urls.py
@@ -4,5 +4,5 @@ from particle import views
 
 urlpatterns = [
     url(r'^$',views.ParticleView.as_view(),name='particle'),
-    url(r'^/(?P<pk>\d+)/(?P<particle_slug>[\w\-]+)$',views.ParticleDetailView.as_view(),name='particle_detail')
+    url(r'^(?P<pk>\d+)/(?P<particle_slug>[\w\-]+)$',views.ParticleDetailView.as_view(),name='particle_detail')
 	]
