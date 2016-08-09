49c2fe96fe833fad08da903f543e365a59b09e8d
Ankit Goyal <goyal.ankit.049@gmail.com>
2016-08-09 22:06:29 +0530

    survey

---
+++ api/serializers.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+from login.models import ExtendedUser
+
+
+class ExtendedUserSerializer(serializers.ModelSerializer):
+    imageUrl = serializers.SerializerMethodField()
+    class Meta:
+        model = ExtendedUser
+        fields = ('id','imageUrl', 'city', 'state', 'country', 'address')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    extendeduser = ExtendedUserSerializer()
+    class Meta:
+        model = User
+        fields = ('id','username', 'email', 'first_name', 'last_name', 'password', 'extendeduser')
+        
+    def create(self, validated_data):
+        print(validated_data)
+        extendeduser_data = validated_data.pop('extendeduser')
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
