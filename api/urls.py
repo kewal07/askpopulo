68f96ca6a5d7d74f8298c7758a7f5f967700b038
Ankit Goyal <goyal.ankit.049@gmail.com>
2016-08-22 20:10:17 +0530

    base 64 change

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,8 @@ import rest_framework.authtoken.views
 from api import views
 
 urlpatterns = [
-	url(r'^gettoken$',rest_framework.authtoken.views.obtain_auth_token,name='gettoken'),
+	#url(r'^gettoken$',rest_framework.authtoken.views.obtain_auth_token,name='gettoken'),
+	url(r'^gettoken$',views.AuthToken.as_view(),name='gettoken'),
 	url(r'^user_detail/(?P<pk>[0-9]+)$', views.UserDetail.as_view(), name='userdetail'),
 	url(r'^survey_list/(?P<user>[0-9]+)', views.getSurveyList, name='getsurveylist'),
 	url(r'^survey_detail/(?P<pk>[0-9]+)', views.SurveyDetail.as_view(), name='getsurveydetail'),
--- api/views.py
+++ api/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponseRedirect,HttpResponse, HttpResponseNotFound
 from api.serializers import UserSerializer, SurveySerializer
 
 import simplejson as json
+import base64
 
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
@@ -15,6 +16,11 @@ from rest_framework import generics
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.authentication import TokenAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from polls.models import Survey
 
@@ -58,3 +64,31 @@ def getSurveyList(request, user, format=None):
 			response_dict["error"] = "No Survey Created by user"
 			return JSONResponse(json.dumps(response_dict), status=400)
 		return JSONResponse(response_dict)
+
+class AuthToken(ObtainAuthToken):
+	def post(self, request, *args, **kwargs):
+		encodedcredentials = request.data.get('credentials')
+		decodedcredentials = base64.b64decode(encodedcredentials).decode('ascii')
+		username, password = decodedcredentials.split(':')
+		tempdict = {'username':username, 'password':password}
+		serializer = self.serializer_class(data=tempdict)
+		serializer.is_valid(raise_exception=True)
+		user = serializer.validated_data['user']
+		token, created = Token.objects.get_or_create(user=user)
+		extendeduser = {}
+		if user.extendeduser.imageUrl:
+			extendeduser['imageUrl'] = str(user.extendeduser.imageUrl)
+		else:
+			extendeduser['imageUrl'] = str(user.extendeduser.imageUrl)
+		extendeduser['birthDay'] = user.extendeduser.birthDay
+		extendeduser['gender'] = user.extendeduser.gender
+		extendeduser['city'] = user.extendeduser.city
+		extendeduser['state'] = user.extendeduser.state
+		extendeduser['country'] = user.extendeduser.country
+		extendeduser['bio'] = user.extendeduser.bio
+		extendeduser['profession'] = user.extendeduser.profession
+		extendeduser['user_slug'] = user.extendeduser.user_slug
+		extendeduser['categories'] = user.extendeduser.categories
+		extendeduser['credits'] = user.extendeduser.credits
+		extendeduser['company_id'] = user.extendeduser.company_id
+		return Response({'token': token.key, 'id':user.id, 'username':user.username, 'email': user.email, 'firstname':user.first_name, 'lastname':user.last_name, 'extendeduser':extendeduser})
